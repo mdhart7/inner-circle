a8cdbdfb1480f18adbeb63e34553e8b8907c0021
mdhart7 <mdhart@uchicago.edu>
2025-11-22 06:22:42 +0000

    permenant caption added

--- app/models/circle_member.rb
+++ app/models/circle_member.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: circle_members
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  member_id  :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_circle_members_on_member_id              (member_id)
+#  index_circle_members_on_user_id                (user_id)
+#  index_circle_members_on_user_id_and_member_id  (user_id,member_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (member_id => users.id) ON DELETE => cascade
+#  fk_rails_...  (user_id => users.id) ON DELETE => cascade
+#
 class CircleMember < ApplicationRecord
   belongs_to :user
   belongs_to :member, class_name: "User"
