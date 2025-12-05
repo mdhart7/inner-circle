3ce27cac9b546c25154d49bd557512b437b8f7ac
mdhart7 <mdhart@uchicago.edu>
2025-12-05 23:56:01 +0000

    Fix syntax error in PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,10 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
-
   def index
-    @posts = Post.order(created_at: :desc)
+    if user_signed_in?
+      friend_ids = current_user.circle_friends_ids
+      @posts = Post.where(user_id: friend_ids).order(created_at: :desc)
+    else
+      @posts = Post.none
     end
-  end
-
-  def index
-    friend_ids = current_user.circle_friends_ids
-    @posts = Post.where(user_id: friend_ids).order(created_at: :desc)
   end
 end
