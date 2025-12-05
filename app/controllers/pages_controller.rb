830a9127a7696b98671d4c1e462ea0c00100b39c
mdhart7 <mdhart@uchicago.edu>
2025-12-05 23:48:18 +0000

    feed update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
+    @posts = Post.order(created_at: :desc)
+    end
+  end
+
+  def index
     friend_ids = current_user.circle_friends_ids
     @posts = Post.where(user_id: friend_ids).order(created_at: :desc)
   end
