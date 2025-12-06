66af114a0f40cd5fe543aa65d2b4fe88b75d648c
mdhart7 <mdhart@uchicago.edu>
2025-12-06 00:06:44 +0000

    Show user posts in feed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
   def index
     if user_signed_in?
       friend_ids = current_user.circle_friends_ids
-      @posts = Post.where(user_id: friend_ids).order(created_at: :desc)
+
+      @posts = Post.where(user_id: [current_user.id] + friend_ids)
+                   .order(created_at: :desc)
     else
       @posts = Post.none
     end
